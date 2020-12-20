2920eb29072948b80189c12b6814f939818bea58
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-12-20 18:00:33 +0200

    Create 01. Basic Stack Operations.cs

---
+++ 01. Stacks and Queues -  Exercises/01. Basic Stack Operations.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+            int s = input[1];
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            int x = input[2];
+
+            if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+
+            else if (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            
+            else 
+            {
+                Console.WriteLine(0);
+            }
+            
+        }
+    }
+}
