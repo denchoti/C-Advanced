5ff9c9c271796d4f8e47cb71303acb20eeb8a785
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-01-15 19:31:44 +0200

    Create 03. Primary Diagonal.cs

---
+++ 02. Multidimensional Arrays - Lab/03. Primary Diagonal.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = ReadMatrix(n, n);
+            primaryDiagonalSum(matrix, n);
+        }
+        static void primaryDiagonalSum(int[,] matrix, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    
+                    if (i == j)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+
+        static int[,] ReadMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            return matrix;
+        }
+    }
+}
