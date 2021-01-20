31350ea980688dd378d7ffd4fadcd1534ff0d3e8
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-01-20 17:32:42 +0200

    Create 05. Snake Moves.cs

---
+++ 02. Multidimensional Arrays - Exercise/05. Snake Moves.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+
+namespace _05.SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            string name = Console.ReadLine();
+
+            char[,] matrix = new char[rows, cols];
+            int currentLetter = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 ==0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        matrix[row, col] = name[currentLetter];
+                        currentLetter++;
+
+                        if (currentLetter == name.Length)
+                        {
+                            currentLetter = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = name[currentLetter];
+                        currentLetter++;
+
+                        if (currentLetter == name.Length)
+                        {
+                            currentLetter = 0;
+                        }
+                    }
+                }
+                
+            }
+            PrintMatrix(matrix);
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
