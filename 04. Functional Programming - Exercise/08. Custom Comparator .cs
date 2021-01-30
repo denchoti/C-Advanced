a62f4eb8b27eddf430449a6061791fb5742539e3
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-01-30 12:17:46 +0200

    Create 08. Custom Comparator .cs

---
+++ 04. Functional Programming - Exercise/08. Custom Comparator .cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+
+namespace _08.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Array.Sort(numbers, (x, y) =>
+            {
+                int sorter = 0;
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    sorter = -1;
+                }
+                else if (x % 2 != 0 && y % 2 == 0)
+                {
+                    sorter = 1;
+                }
+                else
+                {
+                    sorter = x.CompareTo(y);
+                }
+                return sorter;
+            });
+
+            Console.WriteLine(string.Join(" ",numbers));
+
+        }
+    }
+}
