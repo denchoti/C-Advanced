14f75885423efe08027e4426335d02ff3fa9c45e
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-01-30 12:17:24 +0200

    Create 07. Predicate For Names.cs

---
+++ 04. Functional Programming - Exercise/07. Predicate For Names.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            names = names.Where(x => x.Length <= n).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+            
+
+        }
+    }
+}
