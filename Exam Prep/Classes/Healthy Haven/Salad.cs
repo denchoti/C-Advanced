34a86cad3a60a9d83d0b3d119cfd52c15f9edb02
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-02-07 15:25:23 +0200

    Add files via upload

---
+++ Exam Prep/Classes/Healthy Haven/Salad.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthyHaven
+{
+    class Salad
+    {
+        List<Vegetable> products = new List<Vegetable>();
+
+        public Salad(string name)
+        {
+            Name = name;
+            products = new List<Vegetable>();
+        }
+
+        public string Name { get; set; }
+
+        public int GetTotalCalories()
+        {
+            return products.Select(x => x.Calories).Sum();
+        }
+        public int GetProductCount()
+        {
+            return products.Count;
+        }
+        public void Add(Vegetable product)
+        {
+            products.Add(product);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            int calories = GetTotalCalories();
+            
+            sb.AppendLine($"* Salad {Name} is {calories} calories and have {products.Count} products:");
+            foreach (var item in products)
+            {
+                sb.AppendLine(item.ToString().TrimEnd());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exam Prep/Classes/Healthy Haven/Vegetable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthyHaven
+{
+    class Vegetable
+    {
+        public Vegetable(string name, int calories)
+        {
+            Name = name;
+            Calories = calories;
+        }
+
+        public string Name { get; set; }
+        public int Calories { get; set; }
+
+        public override string ToString()
+        {
+            return $" - {Name} have {Calories} calories";
+        }
+    }
+}
