204765c5e64d6412470a2ef88a04e8ed195f1cbf
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-02-10 16:09:21 +0200

    Add files via upload

---
+++ Exam Prep/Classes/Santa's Bag of Presents/Bag.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SantasBagOfPresents
+{
+    public class Bag
+    {
+        private List<Present> bag;
+
+        public Bag(string color, int capacity)
+        {        
+            Color = color;
+            Capacity = capacity;
+            bag = new List<Present>();
+        }
+
+        public string Color { get; set; }
+        public int Capacity { get; set; }
+
+        public void Add(Present present)
+        {
+            if (bag.Count + 1 <= Capacity)
+            {
+                bag.Add(present);
+            }
+        }
+        public bool Remove(string name)
+        {
+            foreach (var present in bag)
+            {
+                if (present.Name == name)
+                {
+                    bag.Remove(present);
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public Present GetHeaviestPresent()
+        {
+           return bag.OrderByDescending(x => x.Weight).FirstOrDefault();
+        
+        }
+
+        public Present GetPresent(string name)
+        {
+            return bag.Where(x => x.Name == name).FirstOrDefault();
+           
+        }
+
+        public int Count => bag.Count;
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} bag contains:");
+         
+            foreach (var present in bag)
+            {
+                sb.AppendLine($"Present {present.Name} for a {present.Gender}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
