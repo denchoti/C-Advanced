40a2cb025852f6640f3e8876158bb8d7aa69d572
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-02-14 19:12:07 +0200

    Add files via upload

---
+++ 06. Generics - Exercise/04. Generic Swap Method Integers/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodIntegers
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            Type valueType = Value.GetType();
+            string valueTypeFullName = valueType.FullName;
+
+
+            return $"{valueTypeFullName}: {this.Value}";
+        }
+    }
+}
