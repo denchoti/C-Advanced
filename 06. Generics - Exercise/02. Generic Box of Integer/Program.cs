a364cb6d7134359eafdf22091f0dc753db163627
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-02-14 19:03:13 +0200

    Create Program.cs

---
+++ 06. Generics - Exercise/02. Generic Box of Integer/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+      
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            Type valueType = Value.GetType();
+            string valueTypeFullName = valueType.FullName;
+
+
+            return $"{valueTypeFullName}: {Value}";
+        }
+    }
+}
