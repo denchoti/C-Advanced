2f8bb5339ee194bbfb4d1a2498715e43043ad232
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 16:26:34 +0200

    Add files via upload

---
+++ 05. Defining Classes - Exercise/06. Speed Racing/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = 0.0;
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+
+        public void Drive(int amountOfKm)
+        {
+            double fuelExpense = FuelConsumptionPerKilometer * amountOfKm;
+
+            if (FuelAmount >= fuelExpense)
+            {
+                FuelAmount -= fuelExpense;
+                TravelledDistance += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+    }
+}
