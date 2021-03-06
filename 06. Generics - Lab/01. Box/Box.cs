6d36cbffb978a2c1124eb89b98d83d55ed332f4b
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 16:32:42 +0200

    Add files via upload

---
+++ 06. Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        Stack<T> elements;
+
+        public BoxOfT()
+        {
+            elements = new Stack<T>();
+        }
+        public void Add(T element)
+        {
+            elements.Push(element);
+        }
+        public T Remove()
+        {
+            
+            return elements.Pop();
+        }
+        public int Count { get { return elements.Count; } }
+    }
+}
