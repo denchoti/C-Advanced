1d1fa55e9341bc74504979b65d75473831a88975
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 16:23:41 +0200

    Add files via upload

---
+++ 05. Defining Classes - Exercise/04. Opinion Poll/Family.cs
@@ -0,0 +1,51 @@
+﻿using DefiningClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _01.DefineClassPerson
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+        public void AddMember (Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            //int maxAge = int.MinValue;
+            //Person person = null;
+            //foreach (var currentPerson in People)
+            //{
+            //    var currentAge = currentPerson.Age;
+            //    if (currentAge > maxAge)
+            //    {
+            //        maxAge = currentAge;
+            //        person = currentPerson;
+            //    }
+            //}
+            //return person; 
+
+            Person oldest = People.OrderByDescending(x => x.Age).First();
+            return oldest;
+            
+        }
+
+        public Person[] GetPeople()
+        {
+            var people = People.Where(x => x.Age > 30)
+                               .OrderBy(x => x.Name)
+                               .ToArray();
+
+            return people;
+        }
+    }
+}
