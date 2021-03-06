a374213a5fdb47688a63608488b60899fdf56d2b
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 16:16:01 +0200

    Add files via upload

---
+++ 05. Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person pesho = new Person("Pesho", 20);
+            Person gosho = new Person("Gosho", 18);
+            Person stamat = new Person("Stamat", 43);
+
+            Console.WriteLine(pesho.Name + " " + pesho.Age);
+        }
+    }
+}
