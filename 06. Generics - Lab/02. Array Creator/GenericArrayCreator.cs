a546b93bca9cc4df224d17ac0a3886a6c3bdd560
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 16:33:36 +0200

    Add files via upload

---
+++ 06. Generics - Lab/02. Array Creator/GenericArrayCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+   public class GenericArrayCreator
+    {
+        public static T[] Create<T>(int length, T defaultItem)
+        {
+            T[] array = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = defaultItem;
+            }
+            return array;
+        }
+    }
+}
