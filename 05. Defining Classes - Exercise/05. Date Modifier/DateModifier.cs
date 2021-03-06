05a5aaa52db820528b33210f103cdc30027ee953
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 16:25:13 +0200

    Add files via upload

---
+++ 05. Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.DefineClassPerson
+{
+    public class DateModifier
+    {
+        public int GetDaysDifference(string startDateString, string endDateString)
+        {
+            DateTime startDate = DateTime.Parse(startDateString);
+            DateTime endDate = DateTime.Parse(endDateString);
+
+            int totalDays = (int)(startDate - endDate).TotalDays;
+            return totalDays;
+        }
+    }
+}
