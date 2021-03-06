a1cf39f5d27c2875a24d3300848a624171f6917e
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 16:31:26 +0200

    Add files via upload

---
+++ 05. Defining Classes - Lab/03. Car Constructors/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelConsumption = 10;
+            FuelQuantity = 200;
+        }
+
+        public Car(string make,string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make,model,year)
+        {
+            FuelConsumption = fuelConsumption;
+            FuelQuantity = fuelQuantity;
+        }
+
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double comsuption = distance * FuelConsumption;
+            if (FuelQuantity - comsuption > 0)
+            {
+                
+                FuelQuantity -= comsuption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
